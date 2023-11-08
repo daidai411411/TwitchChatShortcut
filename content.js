1f54c1c7a4b8c3ad32c1989bd7a1d1b90f8fece3
daidai411411 <daidai411411@gmail.com>
2023-11-08 17:51:16 +0900

    content.jsにコメントを追加

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
 document.addEventListener("keydown", function(event) {
-	if (event.key == "c") {		
+	if (event.key == "c") { // c キーが押されたならば
+		// それぞれのボタンの要素を取得
 		let expand = document.querySelector("[aria-label='チャットを展開']");
 		let collapse = document.querySelector("[aria-label='チャットを折りたたむ']");
 		
+		// ボタンが存在すればクリックする
 		if (expand != null) {
 			expand.click();
 		}
